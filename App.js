2cf9dd809c4c2b810a503fc34aa374dc410ee68e
Anderson <103216079+AndersonMonkouschi@users.noreply.github.com>
2022-07-30 23:30:03 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Entypo, FontAwesome5  } from '@expo/vector-icons';
+
+import TelaInicio from './Componentes/TelaInicio';
+import TelaVitorias from './Componentes/TelaVitorias/';
+import TelaSobre from './Componentes/TelaSobre/';
+
+const Abas = createBottomTabNavigator();
+
+export default function App () {
+  return (
+    <NavigationContainer >
+      <Abas.Navigator screenOptions={{
+        headerStyle:{ backgroundColor: '#eecb01' },
+        headerTitleStyle: { color: '#000', fontWeight: 'bold' },
+
+        tabBarActiveTintColor: '#ba171a',
+        tabBarInactiveTintColor: "#000",
+        tabBarActiveBackgroundColor: "#fff",
+        tabBarStyle: { backgroundColor: "#eecb01"},
+      }}>
+        <Abas.Screen
+          name="Início"
+          component = { TelaInicio }
+          options={{
+            tabBarIcon: ({ color }) => (
+              <Entypo name="home" color={color} size={20} />
+            ),
+          }}
+        />
+
+        <Abas.Screen
+          name="Sobre"
+          component = { TelaSobre }
+          options={{
+            tabBarIcon: ({ color }) => (
+              <FontAwesome5 name="question-circle"  color={color} size={20}/>
+            ),
+          }}
+        />
+
+        <Abas.Screen
+          name="Vitórias"
+          component = { TelaVitorias }
+          options={{
+            tabBarIcon: ({ color }) => (
+              <FontAwesome5 name="trophy" color={color} size={20} />
+            ),
+          }}
+        />
+      </Abas.Navigator>
+    </NavigationContainer>
+
+
+)
+}
+
